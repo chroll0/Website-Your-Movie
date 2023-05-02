261c2f329e13ebf4538084ad5b094892528f122d
Saba Antadze <sabantadze@gmail.com>
2023-05-02 14:04:37 +0400

    make movie site with search bar and database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-// components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import React from "react";
+import "./App.css";
+import { BiSearchAlt } from "react-icons/bi";
+import MovieCard from "./MovieCard";
+
+import { useState, useEffect } from "react";
+const API_URL = "http://www.omdbapi.com?apikey=8521fb6e";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      // Perform search action
+      searchMovies(searchTerm);
+    }
+  }
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
   return (
-    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
-      <Header />
-      <Banner />
-      <Nav />
-      <About />
-      <Services />
-      <Work />
-      <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+    <div className="app">
+      <h1>Your Movie</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          onKeyDown={handleKeyDown}
+        />
+        <BiSearchAlt
+          className="searchBtn"
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
